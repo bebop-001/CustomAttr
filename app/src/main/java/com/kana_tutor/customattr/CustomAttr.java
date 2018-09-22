42bdb7fd1ee1e68221c8b9adc1e70acda66e1c91
sjs <sjs@sonic.net>
2018-09-22 14:40:49 -0700

    Added long click support.

--- app/src/main/java/com/kana_tutor/customattr/CustomAttr.java
+++ app/src/main/java/com/kana_tutor/customattr/CustomAttr.java
@@ -19,13 +19,16 @@ package com.kana_tutor.customattr;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.view.View.OnLongClickListener;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class CustomAttr
         extends AppCompatActivity implements View.OnClickListener{
     private static final String TAG = CustomAttr.class.getSimpleName();
 
+    private static Toast toast;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,15 +37,56 @@ public class CustomAttr
             .setOnClickListener(this);
         findViewById(R.id.button_2)
             .setOnClickListener(this);
-        findViewById(R.id.button_3)
-            .setOnClickListener(this);
+        KanaButton b3 = findViewById(R.id.button_3);
+            b3.setOnClickListener(this);
+            b3.setOnLongClickListener(new OnLongClickListener(){
+                @Override
+                public boolean onLongClick(View v) {
+                    int id = v.getId();
+                    KanaButton b = (KanaButton) v;
+                    String s = "long click:" +
+                            ((id == R.id.button_1) ? "button 1:" : "button 2:") + b.type;
+                    toast.cancel();
+                    toast = Toast.makeText(v.getContext()
+                            , s, Toast.LENGTH_LONG);
+                    toast.show();
+                    return true; // return true or onClick is also called.
+                }
+            });
+        toast = new Toast(this);
     }
 
     @Override
     public void onClick(View v) {
         int id = v.getId();
         KanaButton b = (KanaButton) v;
-        Log.d(TAG, String.format("%s:%s"
-                , (id == R.id.button_1) ? "button 1:" : "button 2:", b.type));
+        String s = "short click:" +
+                ((id == R.id.button_1) ? "button 1:" : "button 2:") + b.type;
+        toast.cancel();
+        toast = Toast.makeText(v.getContext()
+                , s, Toast.LENGTH_LONG);
+        toast.show();
+    }
+    public boolean blueButtonLongClick(View v) {
+        int id = v.getId();
+        KanaButton b = (KanaButton) v;
+        String s = "long click:" +
+                ((id == R.id.button_1) ? "button 1:" : "button 2:") + b.type;
+        toast.cancel();
+        toast = Toast.makeText(v.getContext()
+                , s, Toast.LENGTH_LONG);
+        toast.show();
+        return true; // return true or onClick is also called.
+    }
+    public boolean redOnLongClick(View v) {
+        int id = v.getId();
+        KanaButton b = (KanaButton) v;
+        String s = "long click:" +
+                ((id == R.id.button_1) ? "button 1:" : "button 2:") + b.type;
+        toast.cancel();
+        toast = Toast.makeText(v.getContext()
+                , s, Toast.LENGTH_LONG);
+        toast.show();
+        return true; // return true or onClick is also called.
     }
 }
--- app/src/main/java/com/kana_tutor/customattr/KanaButton.java
+++ app/src/main/java/com/kana_tutor/customattr/KanaButton.java
@@ -17,14 +17,42 @@
  */
 package com.kana_tutor.customattr;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.res.TypedArray;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
 
-public class KanaButton extends android.support.v7.widget.AppCompatButton {
+import java.lang.reflect.Method;
+
+public class KanaButton extends android.support.v7.widget.AppCompatButton
+        implements View.OnLongClickListener {
     private static final String TAG = KanaButton.class.getSimpleName();
 
-    final String type;
+    private Method resolveMethod(Context c, String name) {
+        Method longClick = null;
+        if (c != null && name != null) {
+            try {
+                Method m = c.getClass().getMethod(name, View.class);
+                if (!m.getReturnType().equals(Boolean.TYPE))
+                    throw new NoSuchMethodException(String.format(
+                        "%s:resolveMethod:onLongClick %s must return boolean"
+                            , TAG, name)
+                    );
+                longClick = m;
+            }
+            catch (NoSuchMethodException e) {
+                e.printStackTrace();
+            }
+        }
+        return longClick;
+    }
+
+    final Method onLongClickCallback;
+    final Class cbClass;
+    final String type, onLongClickName;
     public KanaButton(Context context, AttributeSet attrs) {
         super(context, attrs);
         TypedArray ta = getContext().obtainStyledAttributes(
@@ -32,6 +60,28 @@ public class KanaButton extends android.support.v7.widget.AppCompatButton {
         );
         String s = ta.getString(R.styleable.KanaButton_style_name);
         type = (s != null) ? s : "UNDEFINED";
+        onLongClickName = ta.getString(R.styleable.KanaButton_on_long_click);
+        onLongClickCallback = resolveMethod(context, onLongClickName);
+        cbClass = context.getClass();
+        if (onLongClickCallback != null)
+            setOnLongClickListener(this);
         ta.recycle();
+    }
+
+    @Override
+    public boolean onLongClick(View v) {
+        boolean rv = false;
+        if (onLongClickCallback != null) {
+            try {
+                rv = (boolean)onLongClickCallback.invoke(getContext(), v);
+            }
+            catch (Exception e) {
+                throw new RuntimeException(String.format(
+                        "%s:exception on KanaButtonLongClick:%s:%s"
+                    , TAG, onLongClickName, e.getMessage())
+                );
+            }
+        }
+        return rv;
     }
 }
